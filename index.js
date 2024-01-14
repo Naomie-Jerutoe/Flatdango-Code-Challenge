a3096149014567672bd594a3cc8323e79cb2cc20
Naomi Lagat <naomiejerutoe@gmail.com>
2024-01-14 10:47:02 +0300

    feat:add the display details and display details functions

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function getAndDisplayFilms() {
     .then((res) => res.json())
     .then((films) => {
       movies = films;
-      films.map((film) => {
-        const allFilms = `<li><a href="#">${film.title}</a></li>`;
-        ul.insertAdjacentHTML("beforeend", allFilms);
+      films.forEach((film) => {
+        const filmList = document.createElement("li");
+        filmList.innerHTML = `<a href="#" onclick="displayDetails(${film.id})">${film.title}</a>`;
+        ul.appendChild(filmList);
       });
     })
     .catch((error) => {
@@ -25,24 +26,34 @@ function getandDisplayOneFilm() {
   fetch(`${filmsUrl}films/1`)
     .then((res) => res.json())
     .then((film) => {
-      const availableTickets = film.capacity - film.tickets_sold;
-      const oneFilm = `<div class="film-container">
-        <h2>${film.title}</h2>
-        <div class="box">
-            <img src="${film.poster}"
-                alt="${film.title}">
-        </div>
-        <div class="run-show">
-            <h3>Runtime: ${film.runtime}</h3>
-            <h3>Showtime: ${film.showtime}</h3>
-            <h3>Available Tickets: ${availableTickets}</h3>
-            <button onclick="buyTicket(${availableTickets})">Buy Ticket</button>
-        </div>
-    </div>`;
-      main.insertAdjacentHTML("beforeend", oneFilm);
+      displayDetails(film.id);
     })
     .catch((error) => {
       console.log(error.message);
     });
 }
 getandDisplayOneFilm();
+
+function displayDetails(filmId) {
+  const film = movies.filter((movie) => movie.id == filmId);
+  displayFilmDetails(film);
+}
+
+function displayFilmDetails(films) {
+  films.map((film) => {
+    const availableTickets = film.capacity - film.tickets_sold;
+    const filmDetails = `<div class="film-container">
+          <h2>${film.title}</h2>
+          <div class="box">
+              <img src="${film.poster}" alt="${film.title}">
+          </div>
+          <div class="run-show">
+              <h3>Runtime: ${film.runtime}</h3>
+              <h3>Showtime: ${film.showtime}</h3>
+              <h3 class="tickets">Available Tickets: ${availableTickets}</h3>
+              <button onclick="buyTicket(${film.id}, ${availableTickets})">Buy Ticket</button>
+          </div>
+      </div>`;
+    main.innerHTML = filmDetails;
+  });
+}
